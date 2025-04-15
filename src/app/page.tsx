7300ed0ae1ca9be4bf732080097e6ed8b435cc84
dhanushd-27 <dhanushsai2005@gmail.com>
2025-04-15 21:08:19 +0530

    Add: Zustand and Token Store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,27 @@
 "use client";
 
 import { handleSession } from "@/actions/handleSession";
+import { useTokenStore } from "@/lib/store/tokenStore";
 import { isErrorResponse } from "@/utils/api-response-handler";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
+  const { setAccessToken } = useTokenStore()
 
   useEffect(() => {
     const init = async () => {
       const response = await handleSession();
-        router.push('/dashboard')
       if (!isErrorResponse(response)) {
-        
+        const { sessionToken } = response.data as { sessionToken: string};
+        setAccessToken(sessionToken);
+        router.push('/dashboard');
       }
     };
 
     init();
-  }, []);
+  });
 
   return <>
     Landing page
