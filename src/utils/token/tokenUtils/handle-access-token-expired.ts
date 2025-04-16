ba00ea4c0c3024004cc937b0376f23a96f9164a6
dhanushd-27 <dhanushsai2005@gmail.com>
2025-04-16 16:43:58 +0530

    Add: Middleware check and handle access token expiry check

--- src/utils/token/tokenUtils/handle-access-token-expired.ts
+++ src/utils/token/tokenUtils/handle-access-token-expired.ts
@@ -2,6 +2,8 @@ import { cookies } from "next/headers"
 import { verifyRefreshToken } from "../verifyTokens/verify-refresh-token";
 import { accessTokenName, refreshTokenName } from "@/utils/env/env";
 import { createAccessToken } from "../generateTokens/generate-access-token";
+import { createRefreshToken } from "../generateTokens/generate-refresh-token";
+import { prisma } from "@/utils/prisma";
 
 // if access token expire -> take the refresh token veryify if it expired or not if it is logout, if it is not generate new access token and set the access token
 export const handleAccessTokenExpiry = async () => {
@@ -13,13 +15,35 @@ export const handleAccessTokenExpiry = async () => {
 
   if(!isRefreshTokenValid) return null;
 
+  const id = isRefreshTokenValid.id as string;
+  const email = isRefreshTokenValid.email as string;
+
   const newAccessToken = await createAccessToken(refreshToken.value) as string;
+  const newRefreshToken = await createRefreshToken({
+    id: id,
+    email: email
+  });
+
+  (await cookies()).set(refreshTokenName, newRefreshToken, {
+    name: refreshTokenName,
+    httpOnly: true,
+    sameSite: 'lax'
+  });
 
   (await cookies()).set(accessTokenName, newAccessToken, {
     name: accessTokenName,
     httpOnly: true,
     sameSite: "lax",
-  })
+  });
 
+  await prisma.user.update({
+    where: {
+      email: email
+    },
+    data: {
+      refreshToken: newRefreshToken
+    }
+  });
+  
   return newAccessToken;
 }
