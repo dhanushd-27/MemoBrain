400a194a9bf326a15b6b8d6c54a3330d81200d48
dhanushd-27 <dhanushsai2005@gmail.com>
2025-04-16 20:04:17 +0530

    Add: Delete Brain Action

---
+++ src/actions/delete/delete-brain.ts
@@ -0,0 +1,29 @@
+"use server"
+
+import { Status } from "@/types/status-code.types";
+import { ApiError } from "@/utils/api-error";
+import { ApiResponse } from "@/utils/api-response-handler";
+import { AsyncHandler } from "@/utils/async-handler";
+import { accessTokenName } from "@/utils/env/env"
+import { prisma } from "@/utils/prisma";
+import { verifyAccessToken } from "@/utils/token/verifyTokens/verify-access-token";
+import { cookies } from "next/headers"
+
+export const deleteBrain = AsyncHandler(async (brainId: number) => {
+  const token = (await cookies()).get(accessTokenName)?.value as string;
+
+  const payload = await verifyAccessToken(token);
+
+  if(!payload) throw new ApiError(Status.Unauthorized, "Umauthorized User");
+
+  const adminId = payload.id as string;
+
+  await prisma.content.delete({
+    where: {
+      adminId,
+      id: brainId
+    }
+  })
+
+  return ApiResponse(Status.Success, "Brain Deleted Successfully")
+});
