f527992597f6cf7712437c3aa17ce5007a35d571
dhanushd-27 <dhanushsai2005@gmail.com>
2025-04-18 22:41:21 +0530

    Update: Get Brain Action

--- src/actions/get/get-brain-with-url.ts
+++ src/actions/get/get-brain-with-url.ts
@@ -7,18 +7,26 @@ import { AsyncHandler } from "@/utils/async-handler"
 import { prisma } from "@/utils/prisma";
 
 export const getBrainWithUrl = AsyncHandler( async (shareUrl: string) => {
-  const brainData = await prisma.content.findFirst({
-    where: {
-      shareUrl
-    }
+  const brainData = await prisma.$transaction(async (tx) => {
+    const user = await tx.user.findUnique({
+      where: {
+        shareUrl
+      },
+      select: {
+        id: true
+      }
+    });
+
+    if(!user) throw new ApiError(Status.NotFound, "Brain Not Found");
+
+    return await prisma.content.findMany({
+      where: {
+        adminId: user.id
+      }
+    })
   });
-
-  if(!brainData) throw new ApiError(Status.NotFound, "Brain Not Found");
-
+  
   return ApiResponse(Status.Accepted, "Brain Found", {
-    type: brainData.type,
-    tags: brainData.tags,
-    url: brainData.url,
-    title: brainData.title,
-  })
+    brains: brainData
+  });
 });
