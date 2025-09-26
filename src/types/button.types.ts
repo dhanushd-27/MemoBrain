cf09dd87330b225e98f5ec1b44ca4fb0c63361ab
dhanushd-27 <dhanushsai2005@gmail.com>
2025-09-26 20:27:54 +0530

    Remove everything

--- src/types/button.types.ts
+++
@@ -1,7 +0,0 @@
-export type PrimaryButtonProps = {
-  buttonName: string,
-  url: string,
-  target: string,
-}
-
-export type SecondaryButtonProps = PrimaryButtonProps;
--- src/types/response-request.types.ts
+++
@@ -1,9 +0,0 @@
-import { ErrorType } from "./error.types";
-
-export type ResponseType = {
-  status: number,
-  message: string,
-  data: unknown
-}
-
-export type ActionResponse = ResponseType | ErrorType
