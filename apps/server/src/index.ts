1a432833f14b97cf3fe1d2b9c25e066e1c90b3e2
dhanushd-27 <dhanushsai2005@gmail.com>
2025-09-27 22:28:30 +0530

    Create a basic http server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import express from "express"
+import type { Request, Response } from "express"
+
+const app = express()
+
+app.get("/", function(req: Request, res: Response) {
+  res.send("Hello World")
+})
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000")
+})
