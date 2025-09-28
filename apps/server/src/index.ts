3b9ad571fdc1e2fa1f3309e248fc8b999ba405ba
dhanushd-27 <dhanushsai2005@gmail.com>
2025-09-28 16:51:58 +0530

    Add Workflows and Scripts

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,12 +1,12 @@
-import express from "express"
-import type { Request, Response } from "express"
+import express from "express";
+import type { Request, Response } from "express";
 
-const app = express()
+const app = express();
 
-app.get("/", function(req: Request, res: Response) {
-  res.send("Hello World")
-})
+app.get("/", (req: Request, res: Response) => {
+	res.send("Hello World");
+});
 
-app.listen(3000, function() {
-  console.log("Server is running on port 3000")
-})
+app.listen(3000, () => {
+	console.log("Server is running on port 3000");
+});
