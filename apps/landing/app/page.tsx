17dc4f79f12d24a3c8a66e3091beeb217df7ee1e
dhanushd-27 <dhanushsai2005@gmail.com>
2025-12-03 19:26:05 +0530

    Add a Header Desing

--- apps/landing/app/page.tsx
+++ apps/landing/app/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HeaderDesign from '../components/Header/HeaderDesign'
 
 export default function App() {
   return (
-    <div>
-
-    </div>
+    <section className='flex flex-col items-center justify-center h-screen'>
+      <HeaderDesign />
+    </section>
   )
 }
---
+++ apps/landing/components/Header/HeaderDesign.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function HeaderDesign() {
+  return (
+    <section className='relative flex skew-y-6 bg-secondary opacity-50 h-30 w-60 blur-2xl'>
+      <div className='absolute top-0 left-0 w-4 h-full bg-primary z-10'>
+
+      </div>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </section>
+  )
+}
