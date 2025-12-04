8a4d44df5c31bfa77d9773e38cd5b4a388210627
dhanushd-27 <dhanushsai2005@gmail.com>
2025-12-04 23:16:33 +0530

    Create a package of assets and export it

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@repo/tailwind-theme"],
+  transpilePackages: ["@repo/tailwind-theme", "@assets"],
 };
 
 export default nextConfig;
