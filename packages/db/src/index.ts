c7ce7d807f3df3e2ca2e34d438797867c60e0625
dhanushd-27 <dhanushsai2005@gmail.com>
2026-02-02 22:22:22 +0530

    fix(db): resolve ERR_UNSUPPORTED_DIR_IMPORT by adding explicit .js extensions

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,15 +1,15 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-import * as schema from "./schema";
+import * as schema from "./schema/index.js";
 
 const db = drizzle(process.env.DATABASE_URL as string, { schema });
 
 export { db, schema };
-export * from "./schema";
+export * from "./schema/index.js";
 
 // Explicit exports for better TypeScript resolution
-export { users, userRelations } from "./schema/user";
-export { slices, sliceRelations } from "./schema/slice";
-export { sliceAccess, sliceAccessRelations } from "./schema/sliceAccess";
-export { memos, memoRelations, memoTypeEnum } from "./schema/memo";
-export { refreshTokens, refreshTokenRelations } from "./schema/refreshToken";
-export * from "./types";
+export { users, userRelations } from "./schema/user.js";
+export { slices, sliceRelations } from "./schema/slice.js";
+export { sliceAccess, sliceAccessRelations } from "./schema/sliceAccess.js";
+export { memos, memoRelations, memoTypeEnum } from "./schema/memo.js";
+export { refreshTokens, refreshTokenRelations } from "./schema/refreshToken.js";
+export * from "./types.js";
